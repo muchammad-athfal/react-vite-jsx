b7995cea24d28bf3e8ef0da14a536d3043642b25
Muchammad Athfal <muchammad.athfal@gmail.com>
2023-10-09 04:10:21 +0700

    feat: Redux Toolkit

---
+++ redux-toolkit-slice.js
@@ -0,0 +1,27 @@
+import toolkit from "@reduxjs/toolkit";
+
+const { configureStore, createSlice } = toolkit;
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addToCart(state, action) {
+      state.push(action.payload);
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    cart: cartSlice.reducer,
+  },
+});
+
+console.log("onCreate Store: ", store.getState());
+
+store.subscribe(() => {
+  console.log("Store Change: ", store.getState());
+});
+
+store.dispatch(cartSlice.actions.addToCart({ id: 1, qty: 20 }));
