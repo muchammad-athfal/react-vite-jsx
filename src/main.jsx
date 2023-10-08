026a06212e131543d9bf025c8349289fa102281e
Muchammad Athfal <muchammad.athfal@gmail.com>
2023-10-09 05:22:14 +0700

    feat: Implement React Redux

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import RegisterPage from "./pages/register.jsx";
 import ErrorPage from "./pages/errors/404";
 import ProductsPage from "./pages/products";
 import DetailProductPage from "./pages/detailProduct";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Navbar from "./components/Layouts/Navbar";
 
 const router = createBrowserRouter([
   {
@@ -34,6 +37,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />{" "}
+    <Provider store={store}>
+      <Navbar />
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,91 +1,21 @@
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
-import Button from "../components/Elements/Button";
 import { getProducts } from "../services/product.service";
-import { getUser } from "../services/auth.service";
 import { useLogin } from "../hooks/useLogin";
-
-// const products = [
-//   {
-//     id: 1,
-//     name: "Sepatu Baru",
-//     price: 1500000,
-//     image: "/images/shoes-1.jpg",
-//     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,remaining essentially unchanged.`,
-//   },
-//   {
-//     id: 2,
-//     name: "Sepatu Lama",
-//     price: 1000000,
-//     image: "/images/shoes-2.jpg",
-//     description: `lorem Ipsum is simply dummy text of the printing and typesetting medium`,
-//   },
-// ];
+import TableCart from "../components/Fragments/TableCart";
 
 const ProductsPage = () => {
-  const [cart, setCart] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
   const [products, setProducts] = useState([]);
-  const username = useLogin();
+  useLogin();
 
   useEffect(() => {
-    setCart(JSON.parse(localStorage.getItem("cart")) || []);
-
     getProducts((data) => {
       setProducts(data);
     });
   }, []);
 
-  useEffect(() => {
-    if (products.length > 0 && cart.length > 0) {
-      const sum = cart.reduce((acc, item) => {
-        const product = products.find((product) => product.id === item.id);
-        return acc + product.price * item.qty;
-      }, 0);
-
-      setTotalPrice(sum);
-      localStorage.setItem("cart", JSON.stringify(cart));
-
-      totalPriceRef.current.style.display = "table-row";
-    } else {
-      totalPriceRef.current.style.display = "none";
-    }
-  }, [cart, products]);
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.href = "/login";
-  };
-
-  const handleAddToCart = (id) => {
-    if (cart.find((item) => item.id === id)) {
-      setCart(
-        cart.map((item) =>
-          item.id === id ? { ...item, qty: item.qty + 1 } : item
-        )
-      );
-    } else {
-      setCart([...cart, { id, qty: 1 }]);
-    }
-  };
-
-  // UseRef
-  const cartRef = useRef(JSON.parse(localStorage.getItem("cart")) || []);
-  const totalPriceRef = useRef(null);
-
-  const handleAddToCartRef = (id) => {
-    cartRef.current = [...cartRef.current, { id, qty: 1 }];
-    localStorage.setItem("cart", JSON.stringify(cartRef.current));
-  };
-
   return (
     <Fragment>
-      <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-20">
-        {username}
-        <Button classname="ml-5 bg-black" onClick={handleLogout}>
-          Logout
-        </Button>
-      </div>
       <div className="flex justify-center py-5">
         <div className="w-3/4 flex flex-wrap">
           {products.length > 0 &&
@@ -98,7 +28,6 @@ const ProductsPage = () => {
                 <CardProduct.Footer
                   price={product.price}
                   id={product.id}
-                  handleAddToCart={handleAddToCart}
                 ></CardProduct.Footer>
               </CardProduct>
             ))}
@@ -106,61 +35,7 @@ const ProductsPage = () => {
 
         <div className="w-2/4">
           <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
-          <table className="text-left table-auto border-separate border-spacing-x-5">
-            <thead>
-              <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Qty</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {products.length > 0 &&
-                cart.map((item) => {
-                  const product = products.find(
-                    (product) => product.id === item.id
-                  );
-
-                  return (
-                    <tr key={item.id}>
-                      <td>{product.title.substring(0, 20)} ...</td>
-                      <td>
-                        ${" "}
-                        {product.price.toLocaleString("id-ID", {
-                          styles: "currency",
-                          currency: "IDR",
-                        })}
-                      </td>
-                      <td>{item.qty}</td>
-                      <td>
-                        ${" "}
-                        {(item.qty * product.price).toLocaleString("id-ID", {
-                          styles: "currency",
-                          currency: "IDR",
-                        })}
-                      </td>
-                    </tr>
-                  );
-                })}
-
-              <tr ref={totalPriceRef}>
-                <td colSpan={3}>
-                  <b>Total Price</b>
-                </td>
-                <td>
-                  <b>
-                    ${" "}
-                    {totalPrice.toLocaleString("id-ID", {
-                      styles: "currency",
-                      currency: "IDR",
-                    })}
-                  </b>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <TableCart products={products} />
         </div>
       </div>
     </Fragment>
