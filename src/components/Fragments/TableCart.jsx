b3fd2a9eced78ff76e759b8c9d510a3d90ee27cf
Muchammad Athfal <muchammad.athfal@gmail.com>
2023-10-09 08:02:44 +0700

    feat: Update useReducer

--- src/components/Fragments/TableCart.jsx
+++ src/components/Fragments/TableCart.jsx
@@ -1,13 +1,19 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { DarkMode } from "../../context/DarkMode";
+import {
+  useTotalPrice,
+  useTotalPriceDispatch,
+} from "../../context/TotalPriceContext";
 
 const TableCart = (props) => {
   const { products } = props;
   const cart = useSelector((state) => state.cart.data);
-  const [totalPrice, setTotalPrice] = useState(0);
+  // const [totalPrice, setTotalPrice] = useState(0);
   const totalPriceRef = useRef(null);
   const { isDarkMode } = useContext(DarkMode);
+  const dispatch = useTotalPriceDispatch();
+  const { total } = useTotalPrice();
 
   useEffect(() => {
     if (products.length > 0 && cart.length > 0) {
@@ -16,7 +22,12 @@ const TableCart = (props) => {
         return acc + product.price * item.qty;
       }, 0);
 
-      setTotalPrice(sum);
+      dispatch({
+        type: "UPDATE",
+        payload: {
+          total: sum,
+        },
+      });
       localStorage.setItem("cart", JSON.stringify(cart));
       totalPriceRef.current.style.display = "table-row";
     } else {
@@ -73,7 +84,7 @@ const TableCart = (props) => {
           <td>
             <b>
               ${" "}
-              {totalPrice.toLocaleString("id-ID", {
+              {total.toLocaleString("id-ID", {
                 styles: "currency",
                 currency: "IDR",
               })}
